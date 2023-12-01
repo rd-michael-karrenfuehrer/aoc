423348f01c6b9ddae2f5f3737a3135c532197b59
michael.karrenfuehrer@rewe-digital.com <Michael.Karrenfhrer>
2023-12-01 22:24:05 +0100

    change day 1 solution to recursive calculation

--- lib/day1.ex
+++ lib/day1.ex
@@ -1,24 +1,7 @@
-defmodule Day1 do
-  def solve() do
-    solve(File.read!("input.txt"))
-    |> then(&("Result with no replace: " <> to_string(&1)))
-    |> IO.puts()
+defmodule LineOperation do
+  def nothing(input) when is_bitstring(input), do: input
 
-    solve(File.read!("input2.txt"), &replace/1)
-    |> then(&("Result with replace: " <> to_string(&1)))
-    |> IO.puts()
-  end
-
-  def solve(input, apply \\ &noreplace/1) when is_bitstring(input) and is_function(apply) do
-    String.split(input, "\n")
-    |> Enum.map(fn line -> apply.(line) end)
-    |> Enum.map(fn line -> findNumbers(line) end)
-    |> Enum.reduce(0, fn line, acc -> acc + sumFirstAndLast(line) end)
-  end
-
-  def noreplace(input) when is_bitstring(input), do: input
-
-  def replace(input) when is_bitstring(input) do
+  def stringToNumbers(input) when is_bitstring(input) do
     String.replace(input, "one", "one1one")
     |> String.replace("two", "two2two")
     |> String.replace("three", "three3three")
@@ -29,12 +12,44 @@ defmodule Day1 do
     |> String.replace("eight", "eight8eight")
     |> String.replace("nine", "nine9nine")
   end
+end
+
+defmodule Day1 do
+  def solve() do
+    solve(File.read!("input.txt"))
+    |> then(&("Result with no replace: " <> to_string(&1)))
+    |> IO.puts()
+
+    solve(File.read!("input2.txt"), &LineOperation.stringToNumbers/1)
+    |> then(&("Result with replace: " <> to_string(&1)))
+    |> IO.puts()
+  end
+
+  def solve(input, apply \\ &LineOperation.nothing/1)
+      when is_bitstring(input) and is_function(apply) do
+    String.split(input, "\n")
+    |> sumLines(apply)
+  end
+
+  def sumLines([], apply) when is_function(apply), do: 0
+
+  def sumLines([head | tail], apply) when is_function(apply) do
+    sum =
+      apply.(head)
+      |> findNumbers()
+      |> sumFirstAndLast()
+
+    sum + sumLines(tail, apply)
+  end
 
   def findNumbers(input) when is_bitstring(input) do
     Regex.scan(~r/\d/, input)
-    |> Enum.map(fn match -> hd(match) end)
-    |> Enum.map(fn match -> String.to_integer(match) end)
+    |> List.flatten()
+    |> parseNumbers()
   end
+
+  def parseNumbers([]), do: []
+  def parseNumbers([head | tail]), do: [String.to_integer(head) | parseNumbers(tail)]
 
   def sumFirstAndLast([]), do: 0
   def sumFirstAndLast([head | []]), do: head * 10 + head
