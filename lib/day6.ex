b4864c2f94154fb24ba17f6f3e6d1fc500f54622
michael.karrenfuehrer@rewe-digital.com <Michael.Karrenfhrer>
2023-12-06 19:29:52 +0100

    add day 6 solution

---
+++ lib/day6.ex
@@ -0,0 +1,57 @@
+defmodule Day6 do
+  def solve1(input) do
+    lines =
+      input
+      |> String.split("\n", trim: true)
+      |> Enum.map(&parseNumbers/1)
+
+    parseRaces(Enum.at(lines, 0), Enum.at(lines, 1))
+    |> Enum.reduce(1, fn race, acc ->
+      numStres = getWinningStrats(race) |> Enum.count()
+      acc * numStres
+    end)
+  end
+
+  def solve2(input) do
+    lines =
+      input
+      |> String.replace(" ", "")
+      |> String.split("\n", trim: true)
+      |> Enum.map(&parseNumbers/1)
+
+    parseRaces(Enum.at(lines, 0), Enum.at(lines, 1))
+    |> Enum.reduce(1, fn race, acc ->
+      numStres = getWinningStrats(race) |> Enum.count()
+      acc * numStres
+    end)
+  end
+
+  defp getWinningStrats(race, wait \\ 0)
+  defp getWinningStrats(race, wait) when wait == race.time, do: []
+
+  defp getWinningStrats(race, wait) do
+    traveled = (race.time - wait) * wait
+
+    if traveled <= race.distance do
+      getWinningStrats(race, wait + 1)
+    else
+      [wait | getWinningStrats(race, wait + 1)]
+    end
+  end
+
+  defp parseRaces([], _), do: []
+
+  defp parseRaces([time | tailTime], [distance | tailDistance]) do
+    [%{time: time, distance: distance} | parseRaces(tailTime, tailDistance)]
+  end
+
+  defp parseNumbers(numbers) do
+    Regex.scan(~r/\d+/, numbers)
+    |> List.flatten()
+    |> Enum.map(&String.to_integer/1)
+  end
+end
+
+File.read!("day6_input.txt")
+|> Day6.solve2()
+|> IO.inspect()
---
+++ test/day6_test.exs
@@ -0,0 +1,21 @@
+defmodule Day4Test do
+  use ExUnit.Case
+
+  test "solve1" do
+    input = """
+    Time:      7  15   30
+    Distance:  9  40  200
+    """
+
+    assert Day6.solve1(input) == 288
+  end
+
+  test "solve2" do
+    input = """
+    Time:      7  15   30
+    Distance:  9  40  200
+    """
+
+    assert Day6.solve2(input) == 71503
+  end
+end
